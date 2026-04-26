abca57f50810157f1bf6bf7f8365243f606c0b2c
sohakhaleque-cpu <sohakhaleque@gmail.com>
2026-04-26 16:21:36 +0500

    Update Arun1.js

--- Priyansh/commands/Arun1.js
+++ Priyansh/commands/Arun1.js
@@ -17,11 +17,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("Arun") || 
 react.includes("arun")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐫𝐮𝐧 𝐊𝐮𝐦𝐚𝐫✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦✦\n\n★★᭄ :\n\n✦  ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋`",
+				attachment: fs.createReadStream(__dirname + `/noprefix`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+    api.setMessageReaction("", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
